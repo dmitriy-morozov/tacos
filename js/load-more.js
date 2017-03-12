5a8ac751167d8ffb0d39393e362f917f2e36667d
dmitriy-morozov <dimokin777@mail.ru>
2017-03-12 20:27:05 +0200

    final

--- js/load-more.js
+++ js/load-more.js
@@ -1,15 +1,1 @@
-
-/* Load More */
-
-$(function() {
-    $(".works-list .works-item").slice(0, 4).show();
-    $("#works-loadmore").on('click', function(e) {
-        e.preventDefault();
-        $(".works-list .works-item:hidden").slice(0, 4).slideDown();
-        if ($(".works-list .works-item:hidden").length == 0) {
-            $("#works-loadmore").fadeOut('slow');
-        }
-
-    });
-   }); 
-
+$(function(){$(".works-list .works-item").slice(0,4).show(),$("#works-loadmore").on("click",function(o){o.preventDefault(),$(".works-list .works-item:hidden").slice(0,4).slideDown(),0==$(".works-list .works-item:hidden").length&&$("#works-loadmore").fadeOut("slow")})});
